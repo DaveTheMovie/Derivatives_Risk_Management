d3a05114536bd0758eeef2236dbcc6ca1dd8cb2e
David Romoff <DaveTheMovie@gmail.com>
2024-03-05 13:28:59 -0500

    Update Myfile1.py

--- Myfile1.py
+++ Myfile1.py
@@ -1,16 +1,15 @@
-This is my file.  Many are like it but this one is mine!
+# Import the Streamlit library
+import streamlit as st
 
-Thursday is Thanksgiving.  I'm looking forward to the holiday!
+# Create a title for your app
+st.title('Simple Streamlit App')
 
-After Thanksgiving, it's Black Friday.  Do you plan on shopping a lot???
+# Create a text element
+st.write('This is a simple app that squares a number.')
 
-<<<<<<< Updated upstream
-I do plan on shopping.  I want to buy a new suit!
-=======
-And what are you going to do after Black Friday??
->>>>>>> Stashed changes
+# Ask the user to input a number
+number = st.number_input('Insert a number')
 
-I am making a change! this is what I have to say!!!
-
-this is my forked work.
-I have a lot to offer here!
+# Square the number and display the result
+result = number * number
+st.write('The square of the number is: ', result)
