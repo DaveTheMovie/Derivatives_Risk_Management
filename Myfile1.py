4db3a23ddd101d0d5fa6f2647e5a34edbb9b922a
David Romoff <DaveTheMovie@gmail.com>
2024-03-05 13:53:13 -0500

    Update Myfile1.py

--- Myfile1.py
+++ Myfile1.py
@@ -1,5 +1,10 @@
 # Import the Streamlit library
 import streamlit as st
+import random
+
+deltas = [100,150,200,250,300,400,500]
+gammas = [.1,.5,1,1.5,2,5,10,20]
+changes = [1,2,3,4,5]
 
 def change(ds = .01, delta = 100, gamma = 0):
     d0 = delta
@@ -10,16 +15,23 @@ def change(ds = .01, delta = 100, gamma = 0):
     return (delta + .5 * gamma/.01 * ds*100 ) * ds
 
 # Create a title for your app
-st.title('Derivatives Risk Management Trainer')
-
-asdf = 1
+st.title('Derivatives Risk Management')
+st.title('Delta Gamma Trainer')
 
 # Create a text element
 st.write(f'This is a simple app that squares a {asdf}.')
 
-# Ask the user to input a number
-number = st.number_input('Insert a number')
 
-# Square the number and display the result
-result = number * number
-st.write('The square of the number is: ', result)
+delta = random.choice(deltas)
+gamma = random.choice(gammas)
+ds = random.choice(changes)
+
+# Ask the user for the sum
+user_answer = st.number_input(f"What is the change in asset value given a {ds} percent change in the underlying and a delta of {delta} and a gamma of {gamma} ")
+
+# Check if the user's answer is correct
+answer = change(ds = ds/100, delta = delta, gamma = gamma)
+if abs(float(user_answer) - answer) < .0001:
+    st.write(("Correct!")
+else:
+    st.write((f"Incorrect. The correct answer is {answer}.")
