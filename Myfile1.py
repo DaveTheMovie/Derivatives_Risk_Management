b4f4206ac4488f1fce2c71e590285d79cd54ba26
David Romoff <DaveTheMovie@gmail.com>
2024-03-05 13:46:46 -0500

    Update Myfile1.py

--- Myfile1.py
+++ Myfile1.py
@@ -1,11 +1,21 @@
 # Import the Streamlit library
 import streamlit as st
 
+def change(ds = .01, delta = 100, gamma = 0):
+    d0 = delta
+    d1 = delta + gamma/.01 * ds * 100
+    dbar = (d0 + d1)/2
+    dv = dbar * ds
+    print(d0,d1,dbar,dv)
+    return (delta + .5 * gamma/.01 * ds*100 ) * ds
+
 # Create a title for your app
-st.title('Simple Streamlit App')
+st.title('Derivatives Risk Management Trainer')
+
+asdf = 1
 
 # Create a text element
-st.write('This is a simple app that squares a number.')
+st.write(f'This is a simple app that squares a {asdf}.')
 
 # Ask the user to input a number
 number = st.number_input('Insert a number')
